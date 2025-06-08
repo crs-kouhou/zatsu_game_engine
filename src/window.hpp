4a7203e57a97213963b8f20599b9a43a10118b48
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-06-08 18:37:10 +0900

    コメント書いて綺麗にした

--- src/window.hpp
+++ src/window.hpp
@@ -8,6 +8,7 @@ namespace zatsu_ge::window {
 	struct Window final {
 		SDL_Window * window_p;
 
+		// 値の生成
 		static auto make(const std::string title, const std::pair<int, int> size, const SDL_WindowFlags flag) -> std::optional<Window> {
 			if(const auto window_p = SDL_CreateWindow(title.c_str(), size.second, size.first, flag)) {
 				return std::make_optional<Window>(window_p);
@@ -17,20 +18,24 @@ namespace zatsu_ge::window {
 			}
 		}
 
+		// 自明なコンストラクタ
 		Window(SDL_Window * window_p)
 		: window_p{window_p}
 		{}
 
 
+		// 複製ができそうもないので、コピーコンストラクタやコピー代入演算子は削除
 		Window(const Window& rhs) = delete;
 		auto operator=(const Window& rhs) -> Window& = delete;
 
+		// ムーブコンストラクタ
 		Window(Window&& rhs)
 		: window_p{rhs.window_p}
 		{
 			rhs.window_p = nullptr;
 		}
 
+		// ムーブ代入演算子
 		auto operator=(Window&& rhs) -> Window& {
 			if(this->window_p != nullptr) {
 				SDL_DestroyWindow(this->window_p);
@@ -40,19 +45,11 @@ namespace zatsu_ge::window {
 			return *this;
 		}
 
+		// デストラクタ
 		~Window() {
 			if(this->window_p != nullptr) {
 				SDL_DestroyWindow(this->window_p);
 			}
 		}
-	};
-
-	struct SomethingHasWindow final {
-		Window widw;
-
-		SomethingHasWindow(const SomethingHasWindow&) = delete;
-		auto operator=(const SomethingHasWindow&) -> SomethingHasWindow& = delete;
-		SomethingHasWindow(SomethingHasWindow&&) = default;
-		auto operator=(SomethingHasWindow&&) -> SomethingHasWindow& = default;
 	};
 }
