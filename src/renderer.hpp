47475dbc061533787d195a8b7f3061c74af69aa9
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-06-08 18:45:49 +0900

    windowが生きてることの保証は一切しなくていいんですか？ごめんなさい...

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -9,12 +9,40 @@ namespace zatsu_ge::renderer {
 	struct Renderer final {
 		SDL_Renderer * renderer_p;
 
-		static auto make(window::Window windw) -> std::optional<Renderer> {
+		static auto make(const window::Window& windw) -> std::optional<Renderer> {
 			if(const auto renderer_p = SDL_CreateRenderer(windw.window_p, nullptr)) {
 				return std::make_optional<Renderer>(renderer_p);
 			}
 			else {
 				return std::nullopt;
+			}
+		}
+
+		Renderer(SDL_Renderer *const renderer_p)
+		: renderer_p{renderer_p}
+		{}
+
+		Renderer(const Renderer&) = delete;
+		auto operator=(const Renderer&) -> Renderer& = delete;
+
+		Renderer(Renderer&& rhs)
+		: renderer_p{rhs.renderer_p}
+		{
+			rhs.renderer_p = nullptr;
+		}
+
+		auto operator=(Renderer&& rhs) -> Renderer& {
+			if(this->renderer_p) {
+				SDL_DestroyRenderer(this->renderer_p);
+			}
+			this->renderer_p = rhs.renderer_p;
+			rhs.renderer_p = nullptr;
+			return *this;
+		}
+
+		~Renderer() {
+			if(this->renderer_p) {
+				SDL_DestroyRenderer(this->renderer_p);
 			}
 		}
 	};
